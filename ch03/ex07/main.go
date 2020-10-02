30cc874c7ea3c56109fb2a80ed8abd3eb74639e5
mikiya771 <22876341+mikiya771@users.noreply.github.com>
2020-10-02 10:18:00 +0900

    fix: chp03/ex07 add color

--- ch03/ex07/main.go
+++ ch03/ex07/main.go
@@ -27,19 +27,19 @@ func main() {
 func newton(z complex128) color.Color {
 	const iter = 15
 	const contr = 16
-	for i := uint8(0); i<iter; i++{
-		z -= (z - 1/(z*z*z))/4
-		if cmplx.Abs(1-z) < 1e-6{
-			return color.Gray{255-contr * i}
+	for i := uint8(0); i < iter; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(1-z) < 1e-6 {
+			return color.RGBA{0, 0, 255 - contr*i, 255}
 		}
-		if cmplx.Abs(-1-z) < 1e-6{
-			return color.Gray{255-contr * i}
+		if cmplx.Abs(-1-z) < 1e-6 {
+			return color.RGBA{0, 255 - contr*i, 0, 255}
 		}
-		if cmplx.Abs(1i-z) < 1e-6{
-			return color.Gray{255-contr * i}
+		if cmplx.Abs(1i-z) < 1e-6 {
+			return color.RGBA{255 - contr*i, 0, 0, 255}
 		}
-		if cmplx.Abs(-1i-z) < 1e-6{
-			return color.Gray{255-contr * i}
+		if cmplx.Abs(-1i-z) < 1e-6 {
+			return color.RGBA{0, 255 - contr*i, 255 - contr*i, 255}
 		}
 	}
 	return color.Black
